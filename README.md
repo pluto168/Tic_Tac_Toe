d1411a4ebb5613a9c960de008c7152abe6cd7c20
pluto168 <86468019+pluto168@users.noreply.github.com>
2023-11-09 21:58:23 +0800

    part III 完成

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Tic_Tac_Toe
+# 1.Display the gird
+# 2.Accpet user input
+# 3.Update user input
+# 4.Game win-checking algorithm
+# 5.improving game mechanism
--- game.py
+++ game.py
@@ -1,8 +1,8 @@
 counter = 0
 
-row1 = ['','','']
-row2 = ['','','']
-row3 = ['','','']
+row1 = [' ',' ',' ']
+row2 = [' ',' ',' ']
+row3 = [' ',' ',' ']
 
 def display(row1, row2, row3):
     print(row1)
@@ -36,26 +36,98 @@ def getCurrentSymbol():
 def update_table(index):
     global row1, row2, row3
     if index in range(1,4):
-        row1[index - 1] = getCurrentSymbol()
+        if row1[index - 1] == ' ':
+            row1[index - 1] = getCurrentSymbol()
+            return  True
+        else:
+            return False
     elif index in range(4,7):
-        row2[index % 3 - 1 ] = getCurrentSymbol()
+        if row2[index % 3 - 1 ] == ' ':
+            row2[index % 3 - 1 ] = getCurrentSymbol()
+            return True
+        else:
+            return False
     else:
-        row3[index % 3 - 1 ] = getCurrentSymbol()
+        if row3[index % 3 - 1 ] == ' ':
+            row3[index % 3 - 1 ] = getCurrentSymbol()
+            return True
+        else:
+            return False
 
-# update_table(1) #O
-# update_table(4) #X
-# update_table(7) #O
-# update_table(8) #X
-# update_table(2) #O
-
-# display(row1, row2, row3)
-    
-
+def check_winning():
+    player_1_win = False
+    player_2_win = False
+    if (row1[0] == row1[1] and row1[1] == row1[2] and (" " not in row1)):
+        if(row1[0] == "X"):
+            player_2_win = True
+        else:
+            player_1_win = True
+    elif (row2[0] == row2[1] and row2[1] == row2[2] and (" " not in row2)):
+        if(row2[0] == "X"):
+            player_2_win = True
+        elif(row2[0] == "O"):
+            player_1_win = True
+    elif (row3[0] == row3[1] and row3[1] == row3[2] and (" " not in row3)):
+        if(row3[0] == "X"):
+            player_2_win = True
+        elif(row3[0] == "O"):
+            player_1_win = True
+            
+    elif (row1[0] == row2[0] and row2[0] == row3[0] and (row1[0] != " " and row2[0] != " " and row3[0] != " ")):
+        if(row1[0] == "X"):
+            player_2_win = True
+        elif(row1[0] == "O"):
+            player_1_win = True
+    elif (row1[1] == row2[1] and row2[1] == row3[1] and (row1[1] != " " and row2[1] != " " and row3[1] != " ")):
+        if(row1[1] == "X"):
+            player_2_win = True
+        elif(row1[1] == "O"):
+            player_1_win = True    
+    elif (row1[2] == row2[2] and row2[2] == row3[2] and (row1[2] != " " and row2[2] != " " and row3[2] != " ")):
+        if(row1[2] == "X"):
+            player_2_win = True
+        elif(row1[2] == "O"):
+            player_1_win = True   
+                
+    #對角            
+    elif (row1[0] == row2[1] and row2[1] == row3[2] and (row1[0] != " " and row2[1] != " " and row3[2] != " ")):
+        if(row1[0] == "X"):
+            player_2_win = True
+        elif(row1[0] == "O"):
+            player_1_win = True              
+    elif (row1[2] == row2[1] and row2[1] == row3[0] and (row1[2] != " " and row2[1] != " " and row3[0] != " ")):
+        if(row1[2] == "X"):
+            player_2_win = True
+        elif(row1[2] == "O"):
+            player_1_win = True      
+            
+            
+    if player_1_win:
+        return "player_1_win"
+    elif player_2_win:
+        return "player_2_win"
+    else:
+        return "no one wins"
+            
 def start_game():
     while True:
         display(row1, row2, row3)
-        choice = user_choice()
-        update_table(choice)
+        while True:
+            choice = user_choice()
+            if update_table(choice):
+                break
+            else:
+                print("Wrong position to put your choice")
+                
+        result = check_winning()
+        if result == "player_1_win":
+            display(row1, row2, row3)
+            print("Player 1 wins!! congrats")
+            return 
+        elif result == "player_2_win":
+            display(row1, row2, row3)
+            print("Player 2 wins!! congrats")
+            return 
         
 
 start_game()
