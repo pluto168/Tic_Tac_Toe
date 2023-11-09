9c825734079f8fb41e18470ec933ff4ce1386be8
pluto168 <86468019+pluto168@users.noreply.github.com>
2023-11-09 16:56:50 +0800

    partII 製作start game

--- game.py
+++ game.py
@@ -1,3 +1,5 @@
+counter = 0
+
 row1 = ['','','']
 row2 = ['','','']
 row3 = ['','','']
@@ -20,8 +22,40 @@ def user_choice():
         
 # display(row1, row2, row3)
 
-user_choice() 
+# user_choice()
 
+def getCurrentSymbol():
+    global counter 
+    symbol_list = ['X','O'] 
     
+    counter += 1
+    return symbol_list[counter % 2 ]
+
+# print (getCurrentSymbol())
+
+def update_table(index):
+    global row1, row2, row3
+    if index in range(1,4):
+        row1[index - 1] = getCurrentSymbol()
+    elif index in range(4,7):
+        row2[index % 3 - 1 ] = getCurrentSymbol()
+    else:
+        row3[index % 3 - 1 ] = getCurrentSymbol()
+
+# update_table(1) #O
+# update_table(4) #X
+# update_table(7) #O
+# update_table(8) #X
+# update_table(2) #O
+
+# display(row1, row2, row3)
     
 
+def start_game():
+    while True:
+        display(row1, row2, row3)
+        choice = user_choice()
+        update_table(choice)
+        
+
+start_game()
